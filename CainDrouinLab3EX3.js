34bdd703e1c9f764f561e302050282fd2f83b6c3
Cain-Drouin <124199333+Cain-Drouin@users.noreply.github.com>
2023-02-22 14:05:38 -0500

    Added Comments

--- CainDrouinLab3EX3.js
+++ CainDrouinLab3EX3.js
@@ -13,20 +13,24 @@ function mySprite(x, y, size, tColor, eColor, rColor) {
     push();
     translate(x, y);
     scale(size);
+    //Ears
     fill("black");
     ellipse(30, 20, 20, 40);
     ellipse(70, 20, 20, 40);
     fill("chocolate");
     ellipse(70, 20, 10, 30);
     ellipse(30, 20, 10, 30);
+    //face
     fill("black");
     ellipse(50, 50, 80);
     push();
+    //eyes
     fill("white");
     noStroke();
     ellipse(35, 35, 15);
     ellipse(65, 35, 15);
     pop();
+    //nose
     ellipse(35, 35, 5);
     ellipse(65, 35, 5);
     fill("chocolate");
