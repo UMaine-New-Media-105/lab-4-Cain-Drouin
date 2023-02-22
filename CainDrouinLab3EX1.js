ebb249a78dabeb4c97d3242611bd3e36ffd3f519
Cain-Drouin <124199333+Cain-Drouin@users.noreply.github.com>
2023-02-22 13:30:08 -0500

    Added Color Knob

--- CainDrouinLab3EX1.js
+++ CainDrouinLab3EX1.js
@@ -5,20 +5,29 @@ function setup() {
 function draw() {
   background(220);
 
-  myTriangle(100, 100, 100, 200, 200, 100);
-  myEllipse(200, 200, 200);
-  myRoundSquare(0,0,100,10,20,30,50)
+  myTriangle(100, 100, 100, 200, 200, 100,"aqua");
+  myEllipse(200, 200, 200,"red");
+  myRoundSquare(0,0,100,10,20,30,50,"blue")
 }
 
-function myTriangle(x1,y1,x2,y2,x3,y3) {
+function myTriangle(x1,y1,x2,y2,x3,y3,theColor) {
+   push()
+  fill(theColor)
   triangle(x1,y1,x2,y2,x3,y3);
+  pop()
 }
 
-function myEllipse(x,y,d) {
+function myEllipse(x,y,d,theColor) {
+  push()
+  fill(theColor)
   ellipse(x,y,d);
+  pop()
 }
 
-function myRoundSquare(x,y,size,tl,tr,br,bl){
+function myRoundSquare(x,y,size,tl,tr,br,bl,theColor){
+  push()
+  fill(theColor)
   square(x,y,size,tl,tr,br,bl)
+  pop()
   
 }
